413df62a0c9b70f6c4919ef6ed76690131f60492
Yaksh Git <chudasamayaksh77@gmail.com>
2024-10-12 17:36:42 +0530

    Learned about more concepts of pointers

--- Day_14/Pointers/code.cpp
+++ Day_14/Pointers/code.cpp
@@ -38,9 +38,52 @@ int main(){
 
     // ----- Null Pointer -----
     int* nullPtr = NULL;
-    cout << nullPtr << endl;
-    cout << &nullPtr << endl;
+    // cout << nullPtr << endl;
+    // cout << &nullPtr << endl;
+
+    // ----- Array Pointer -----
+    //* array pointers are constant pointers.
+
+    int arr[] = {1,2,3,4,5};
+    // cout << arr << endl;
+    // cout << *arr << endl; // Dereferencing arr pointer
+    // cout << *(arr+1) << endl;
+    // cout << *(arr+2) << endl;
+    // cout << *(arr+3) << endl;
+    // cout << *(arr+4) << endl;
+    // int a = 10;
+    // arr = &a; // It is not possible
 
 
+    // ----- Pointer Arithmetic -----
+    int num = 20;
+    int* p = &a;
+    // cout << p << endl;
+    // p++;
+    // cout << p << endl;
+    // ++p;
+    // cout << p << endl;
+    // --p;
+    // cout << p << endl;
+
+    // p += 2;
+    // cout << p << endl;
+    // cout << &p << endl;
+
+    // ------ Add anf subtract pointers ------
+
+    // int* ptr2;
+    // int* ptr1 = ptr2 + 2;
+    // cout << ptr1 << " " << ptr2 << endl;
+    // cout << ptr1 - ptr2 << endl;
+
+    // ------ Compare pointers ------
+
+    int* ptr1;
+    int* ptr2;
+
+    cout << ptr1 << " " << ptr2 << endl;
+    cout << (ptr1 < ptr2) << endl;
+    cout << (ptr1 > ptr2) << endl;
     return 0;
 }
